5f84e0c06f7c7c23f32558e3d9d00ca720603389
Kirill <scriptythememer@gmail.com>
2025-06-19 21:20:27 +0700

    goal response

---
+++ Code/Game/ItemResponse/GoalResponse.cs
@@ -0,0 +1,29 @@
+[Group("Brozenye/ItemResponse")]
+public class GoalResponse : Component, IItemResponse {
+	[Property, TextArea] public string[] MessageSuccess { get; set; }
+	[Property, TextArea] public string[] MessageFailed { get; set; }
+	[Property, TextArea] public string[] MessageZero { get; set; }
+
+	[Property, Group("Thresholds")] public float SuccessThreshold { get; set; } = 0.6f;
+
+	private GameGoal goal;
+
+	protected override void OnStart() {
+		goal = Scene.GetComponentInChildren<GameGoal>();
+	}
+
+	public void Respond(string name, Dictionary<string, int> alchemy, DialogueNPC npc) {
+		var score = goal.EvaluatePotionScore(alchemy);
+
+
+		if (score > SuccessThreshold) {
+			npc.YapDialogue(MessageSuccess);
+			// todo create some code that ends the game or smth
+		} else if (score <= 0.01) {
+			npc.YapDialogue(MessageZero);
+		} else if (score < SuccessThreshold) {
+			npc.YapDialogue(MessageFailed);
+			Log.Info(score);
+		}
+	}
+}
