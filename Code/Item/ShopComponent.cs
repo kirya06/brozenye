686255511356d8589926ba3ee100d0876fbcb31e
Kirill <scriptythememer@gmail.com>
2025-06-21 15:24:22 +0700

    barebones shop component

---
+++ Code/Item/ShopComponent.cs
@@ -0,0 +1,16 @@
+public class ShopComponent : Component, IInteractable {
+	[Property] public GameObject Item { get; set; }
+	[Property] public int Price { get; set; }
+
+	public void Interact(GameObject source) {
+		var inventory = source.GetComponent<PlayerInventory>();
+		if (inventory == null) return;
+
+		if (inventory.Rubles > Price && !inventory.IsFull()) {
+			inventory.Rubles -= Price;
+
+			var item = Item.Clone();
+			item.GetComponent<ItemComponent>().Interact(source);
+		}
+	}
+}
