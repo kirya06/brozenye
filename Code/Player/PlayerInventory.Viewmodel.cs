056a3430f67a8c65f074d207fa41308c97e833a4
Kirill <scriptythememer@gmail.com>
2025-06-22 22:47:15 +0700

    particle effects

--- Code/Player/PlayerInventory.Viewmodel.cs
+++ Code/Player/PlayerInventory.Viewmodel.cs
@@ -3,9 +3,10 @@ public partial class PlayerInventory {
 	[Property, Feature("Viewmodel")] public Vector3 TargetPosition { get; set; }
 	[Property, Feature("Viewmodel")] public GameObject ViewmodelParent { get; set; }
 
-	[Property, Feature("Viewmodel")] public Dictionary<string, Texture> PotionEffects { get; set; } = new();
+	[Property, Feature("Viewmodel")] public Dictionary<string, GameObject> PotionEffects { get; set; } = new();
 
 	private int lastItemHash;
+	private List<GameObject> particleObj = new();
 
 	private void updateViewmodelPosition() {
 		var wishPosition = TargetPosition * ViewmodelParent.WorldRotation;
@@ -20,23 +21,56 @@ public partial class PlayerInventory {
 		var hash = SelectedItem == null ? 0 : SelectedItem.GetHashCode();
 
 		if (lastItemHash != hash) {
+
+			foreach (var obj in particleObj) {
+				obj.Destroy();
+			}
+			particleObj = new();
+
 			if (SelectedItem != null) {
 				Viewmodel.Model = SelectedItem.Model.Model;
 				Viewmodel.Tint = SelectedItem.Model.Tint;
 				Viewmodel.Enabled = true;
 
 				Viewmodel.LocalScale = SelectedItem.LocalScale;
+
+				if (SelectedItem.Tags.Has("potion")) {
+					setParticles();
+				}
 			} else {
 				Viewmodel.Enabled = false;
 			}
 
 		}
 
-		Log.Info(hash);
 		updateViewmodelPosition();
-		
-
 		lastItemHash = hash;
+	}
+
+	private void setParticles() {
+		foreach (var keyval in SelectedItem.AlchemicProperties) {
+			if (PotionEffects.ContainsKey(keyval.Key)) {
+				var newParticle = PotionEffects[keyval.Key].Clone();
+				newParticle.SetParent(Viewmodel.GameObject, false);
+
+				var particles = newParticle.GetComponent<ParticleEffect>();
+				particles.Tint = Color.Black.LerpTo(Color.Green, (float)keyval.Value / 3f);
+
+				if (keyval.Value == 2) {
+					var newScale = new ParticleFloat();
+					newScale.ConstantValue = 4.5f;
+					particles.Scale = newScale;
+				}
+
+				if (keyval.Value >= 3) {
+					var newScale = new ParticleFloat();
+					newScale.ConstantValue = 6;
+					particles.Scale = newScale;
+				}
+
+				particleObj.Add(newParticle);
+			}
+		}
 	}
 
 }
