d35e96e12d36b4d79401e88e4fe76bb666f400ff
Kirill <scriptythememer@gmail.com>
2025-06-24 21:53:31 +0700

    rewrite feature for RubleReward worm king

--- Code/Game/ItemResponse/RubleReward.cs
+++ Code/Game/ItemResponse/RubleReward.cs
@@ -2,6 +2,14 @@ public class RubleReward : Component, IItemResponse {
 	[Property, TextArea] public string[] Message { get; set; }
 	[Property] public int Reward { get; set; }
 	[Property] public bool DoItOnce { get; set; }
+
+	/// <summary>
+	/// When giving an item you rewrite the base NPC dialogue (the one that comes out on interact)
+	/// </summary>
+	[Property, Feature("Rewrite")] public bool EnableRewrite { get; set; }
+	[Property, Feature("Rewrite")] public bool UseRewardResponse { get; set; } = true;
+	[Property, Feature("Rewrite"), HideIf("UseRewardResponse", true)] public string[] RewrittenMessage { get; set; }
+
 	public void Respond(string itemName, Dictionary<string, int> alchemy, DialogueNPC npc = null) {
 		npc.YapDialogue(Message);
 
@@ -10,6 +18,10 @@ public class RubleReward : Component, IItemResponse {
 		if (DoItOnce) {
 			var reciever = GameObject.GetComponent<ItemReciever>();
 			reciever.ItemWhitelist.Remove(itemName);
+		}
+
+		if (EnableRewrite) {
+			npc.Dialogue = UseRewardResponse ? Message : RewrittenMessage;
 		}
 	}
 }
